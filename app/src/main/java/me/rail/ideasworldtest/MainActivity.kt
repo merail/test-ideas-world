8231ba5a28bd2b4a6f7921654284a64817079fc6
merail <rail.me@google.com>
2022-01-28 18:23:55 +0300

    Preparing

--- app/src/main/java/me/rail/ideasworldtest/MainActivity.kt
+++ app/src/main/java/me/rail/ideasworldtest/MainActivity.kt
@@ -2,10 +2,14 @@ package me.rail.ideasworldtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import me.rail.ideasworldtest.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
